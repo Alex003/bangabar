8b6af19d23be498ac058c0567ae1aff72359e840
Lebed Tima <lebed.tima@gmail.com>
2014-07-31 13:10:37 +0300

    setup and config mandrill

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
             new AlexanderC\Bundle\UngaBungaBundle\UngaBungaBundle(),
+            new Hip\MandrillBundle\HipMandrillBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
